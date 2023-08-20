1110af0dc6db9a0f232888e715cc72c8b1db2ea0
Aftaza <taftazani17@student.ub.ac.id>
2023-08-20 15:22:47 +0700

    Update route 30%

--- app/Http/Controllers/Frontsite/LandingController.php
+++ app/Http/Controllers/Frontsite/LandingController.php
@@ -20,16 +20,6 @@ use App\Models\User;
 class LandingController extends Controller
 {
     /**
-     * Create a new controller instance
-     * 
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
